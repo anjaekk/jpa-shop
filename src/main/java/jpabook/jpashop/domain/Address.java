6a82035c503cc6311a386e0faa6df74b23b119b7
An jaegyeong <anjaekk@gmail.com>
2023-10-18 10:12:08 +0900

    fix: Address embeddable 값 타입 수정

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -3,6 +3,11 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 
+/**
+ * 값 타입은 변경 불가능하게 설계해야 함
+ * JPA 스팩상 임베디드타입(@Embeddable)은 자바 기본 생성자를 public 혹은 protected로 설정해야한다.
+ * (reflection 사용을 위해)
+ */
 @Embeddable
 @Getter
 public class Address {
@@ -10,4 +15,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    // reflection 등을 사용하기 위해서 기본 생성자 필수
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
